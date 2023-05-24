9975e97000fe1c2c585df512603e96ab6860b4b0
frank-Mwangi <kungofrank@gmail.com>
2023-05-24 13:24:12 +0300

    Master and slave configuration files

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Record all tasks owned by an employee
+Export this data in JSON format
+"""
+
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                         .format(id).json())
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(id).json())
+
+    with open("{}.json".format(id), "w") as userId:
+        json.dump({id: [{
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': users.get('username')
+        } for task in tasks]}, id)
