acd59bb6319454f796b87e9cc7ff15c602f30a84
frank-Mwangi <kungofrank@gmail.com>
2023-06-15 10:12:15 +0300

    Increase ulimit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,8 @@
 # Increase the number of requests web server
 # can handle by increasing ulimit
 
-exec { 'add-requests':
-  command  => 'sed -i "5s/[0-9]\+$( ulimit -n )/" /etc/default/nginx; sudo service nginx restart',
+exec { 'file limit':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
   provider => shell,
 }
