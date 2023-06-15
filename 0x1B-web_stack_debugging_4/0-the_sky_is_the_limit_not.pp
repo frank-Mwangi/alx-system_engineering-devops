f0416c6060b5b81de935c03bc7b99f718c62a030
frank-Mwangi <kungofrank@gmail.com>
2023-06-15 10:14:59 +0300

    Fixing puppet-lint compliance

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 # can handle by increasing ulimit
 
 exec { 'file limit':
-  onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
   provider => shell,
 }
